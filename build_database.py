b65af050a56c3a78d7fdfa55552a66e650da9c25
Jacob Mennell <jacobmennell@outlook.com>
2022-08-01 13:36:16 +0100

    added log file for update function

--- build_database.py
+++ build_database.py
@@ -4,6 +4,7 @@ import requests
 import logging
 import time
 import sqlite3
+import datetime
 
 
 # functions
@@ -94,6 +95,15 @@ file_handler_format = '%(asctime)s | %(levelname)s | %(lineno)d: %(message)s'
 file_handler.setFormatter(logging.Formatter(file_handler_format))
 logger.addHandler(file_handler)
 
+# create dicts of dates from text file for df filtering
+date_dict = {}
+with open("last_update.txt") as f:
+    for line in f:
+        k, v = line.split(' ', 1)
+        v = v[:-1]
+        date_dict[k] = v
+f.close
+
 # import bar data
 bar_stock_df = pd.read_csv('data/bar_data.csv'
                            , header=0
@@ -117,6 +127,8 @@ budapest_df = pd.read_csv('data/budapest.csv.gz'
                           , names=col_names
                           , parse_dates=['dateOfSale'])
 budapest_df['bar'] = 'budapest'
+budapest_df = budapest_df.loc[budapest_df["dateOfSale"] > date_dict['BUDA_date_max']]
+budapest_max_date = str(budapest_df.dateOfSale.max())
 
 # import london data
 london_df = pd.read_csv('data/london_transactions.csv.gz'
@@ -126,6 +138,8 @@ london_df = pd.read_csv('data/london_transactions.csv.gz'
                         , names=col_names
                         , parse_dates=['dateOfSale'])
 london_df['bar'] = 'london'
+london_df = london_df.loc[london_df["dateOfSale"] > date_dict['LON_date_max']]
+london_max_date = str(london_df.dateOfSale.max())
 
 # import new york data
 new_york_df = pd.read_csv('data/ny.csv.gz'
@@ -135,9 +149,22 @@ new_york_df = pd.read_csv('data/ny.csv.gz'
                           , names=col_names
                           , parse_dates=['dateOfSale'])
 new_york_df['bar'] = 'new york'
+new_york_df = new_york_df.loc[new_york_df["dateOfSale"] > date_dict['NYC_date_max']]
+ny_max_date = str(new_york_df.dateOfSale.max())
+
+# set new dates to limit size of future uploads
+f = open('last_update.txt', 'w')
+f.write(f"NYC_date_max {ny_max_date}\nLON_date_max {london_max_date}\nBUDA_date_max {budapest_max_date}\n")
+date_dict = {}
+f =  open("last_update.txt")
+for line in f:
+    k, v = line.split(' ', 1)
+    v = v[:-1]
+    date_dict[k] = v
+f.close
 
 # concat dataframes
-global_df = pd.concat([budapest_df, london_df, new_york_df])
+global_df = pd.concat([budapest_df, london_df, new_york_df], ignore_index=True)
 # minor cleaning
 global_df = global_df.reset_index().rename(columns={'index': 'saleID'}).set_index('saleID')
 global_df['drink'] = global_df['drink'].map(lambda x: x.upper())
@@ -155,6 +182,7 @@ dfs = []
 for drink in master_drinks:
     # extract data from the offset value
     df = cocktail_extract(drink)
+    df = df[['idDrink', 'strDrink', 'strCategory', 'strIBA', 'strAlcoholic', 'strGlass', 'dateModified']]
     # append data
     dfs.append(df)
     # print output to see status
@@ -162,8 +190,10 @@ for drink in master_drinks:
     counter += 1
 
 # combine dfs
-cocktails_df = pd.concat(dfs)
+cocktails_df = pd.concat(dfs, ignore_index=True)
 # minor cleaning
+cocktails_df = cocktails_df.drop_duplicates()
+#cocktails_df = cocktails_df.reset_index().rename(columns={'index': 'cocktailID'}).set_index('cocktailID')
 cocktails_df['strDrink'] = cocktails_df['strDrink'].map(lambda x: x.upper())
 cocktails_df['strGlass'] = cocktails_df['strGlass'].map(lambda x: x.upper())
 
@@ -176,16 +206,12 @@ execute_external_sql_script_file('data_tables.sql', 'bar_db')
 conn = sqlite3.connect('bar_db')
 
 # send each dataframe to sql
-cocktails_df.to_sql('cocktails', conn, if_exists='replace')
-global_df.to_sql('global_sales', conn, if_exists='replace')
-bar_stock_df.to_sql('bar_stock', conn, if_exists='replace')
+global_df.to_sql('global_sales', conn, if_exists='append', index=True, index_label='saleID')
+bar_stock_df.to_sql('bar_stock', conn, if_exists='append', index=True, index_label='stockID')
+cocktails_df.to_sql('cocktails', conn, if_exists='append', index=False)
 
 # Close the connection object.
 conn.close()
 
 # Create POC Table for analysis
 execute_external_sql_script_file('poc_tables.sql', 'bar_db')
-
-# next steps
-# add new data without batch refreshing
-# use date to filter insert in future
