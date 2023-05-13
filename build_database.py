a57929f7299cebceaa4e6e52337fb2fa03c09dfb
Jacob Mennell <67950889+jacob-mennell@users.noreply.github.com>
2023-05-13 12:33:18 +0100

    Update build_database.py

--- build_database.py
+++ build_database.py
@@ -75,141 +75,142 @@ def execute_external_sql_script_file(script_file_path, db_name):
     # Execute the read out sql script string.
     execute_sql_script(sql_script_string, db_name)
 
-
-# initialise log file
-logger = logging.getLogger()
-logger.setLevel(logging.NOTSET)
-
-# return error and critical logs to console
-console = logging.StreamHandler()
-console.setLevel(logging.ERROR)
-console_format = '%(asctime)s | %(levelname)s: %(message)s'
-console.setFormatter(logging.Formatter(console_format))
-logger.addHandler(console)
-
-# create log file to capture all logging
-file_handler = logging.FileHandler('drinks_db.log')
-file_handler.setLevel(logging.INFO)
-file_handler_format = '%(asctime)s | %(levelname)s | %(lineno)d: %(message)s'
-file_handler.setFormatter(logging.Formatter(file_handler_format))
-logger.addHandler(file_handler)
-
-# create dicts of dates from text file for date filtering prior to upload
-date_dict = {}
-with open("last_update.txt") as f:
+if __name__ == "__main__":
+    
+    # initialise log file
+    logger = logging.getLogger()
+    logger.setLevel(logging.NOTSET)
+    
+    # return error and critical logs to console
+    console = logging.StreamHandler()
+    console.setLevel(logging.ERROR)
+    console_format = '%(asctime)s | %(levelname)s: %(message)s'
+    console.setFormatter(logging.Formatter(console_format))
+    logger.addHandler(console)
+    
+    # create log file to capture all logging
+    file_handler = logging.FileHandler('drinks_db.log')
+    file_handler.setLevel(logging.INFO)
+    file_handler_format = '%(asctime)s | %(levelname)s | %(lineno)d: %(message)s'
+    file_handler.setFormatter(logging.Formatter(file_handler_format))
+    logger.addHandler(file_handler)
+    
+    # create dicts of dates from text file for date filtering prior to upload
+    date_dict = {}
+    with open("last_update.txt") as f:
+        for line in f:
+            k, v = line.split(' ', 1)
+            v = v[:-1]
+            date_dict[k] = v
+    f.close
+    
+    # import bar data
+    bar_stock_df = pd.read_csv('data/bar_data.csv'
+                               , header=0
+                               , sep=',')
+    # minor cleaning
+    bar_stock_df = bar_stock_df.reset_index().rename\
+        (columns={'index': 'stockID', 'glass_type': 'glassType'}).set_index('stockID')
+    bar_stock_df['stock'] = bar_stock_df['stock'].str.extract('(\d+)', expand=False)
+    bar_stock_df['stock'] = bar_stock_df['stock'].astype(int)
+    bar_stock_df = bar_stock_df.applymap(lambda s: s.lower() if type(s) == str else s)
+    
+    # set global df column names
+    col_names = ['dateOfSale', 'drink', 'price']
+    
+    # import budapest data
+    budapest_df = pd.read_csv('data/budapest.csv.gz'
+                              , compression='gzip'
+                              , header=0
+                              , sep=','
+                              , names=col_names
+                              , parse_dates=['dateOfSale'])
+    budapest_df['bar'] = 'budapest'
+    budapest_df = budapest_df.loc[budapest_df["dateOfSale"] > date_dict['BUDA_date_max']]
+    budapest_max_date = str(budapest_df.dateOfSale.max())
+    
+    # import london data
+    london_df = pd.read_csv('data/london_transactions.csv.gz'
+                            , compression='gzip'
+                            , header=None
+                            , sep='\t'
+                            , names=col_names
+                            , parse_dates=['dateOfSale'])
+    london_df['bar'] = 'london'
+    london_df = london_df.loc[london_df["dateOfSale"] > date_dict['LON_date_max']]
+    london_max_date = str(london_df.dateOfSale.max())
+    
+    # import new york data
+    new_york_df = pd.read_csv('data/ny.csv.gz'
+                              , compression='gzip'
+                              , header=0
+                              , sep=','
+                              , names=col_names
+                              , parse_dates=['dateOfSale'])
+    new_york_df['bar'] = 'new york'
+    new_york_df = new_york_df.loc[new_york_df["dateOfSale"] > date_dict['NYC_date_max']]
+    ny_max_date = str(new_york_df.dateOfSale.max())
+    
+    # set new dates to limit size of future uploads
+    f = open('last_update.txt', 'w')
+    f.write(f"NYC_date_max {ny_max_date}\nLON_date_max {london_max_date}\nBUDA_date_max {budapest_max_date}\n")
+    date_dict = {}
+    f = open("last_update.txt")
     for line in f:
         k, v = line.split(' ', 1)
         v = v[:-1]
         date_dict[k] = v
-f.close
-
-# import bar data
-bar_stock_df = pd.read_csv('data/bar_data.csv'
-                           , header=0
-                           , sep=',')
-# minor cleaning
-bar_stock_df = bar_stock_df.reset_index().rename\
-    (columns={'index': 'stockID', 'glass_type': 'glassType'}).set_index('stockID')
-bar_stock_df['stock'] = bar_stock_df['stock'].str.extract('(\d+)', expand=False)
-bar_stock_df['stock'] = bar_stock_df['stock'].astype(int)
-bar_stock_df = bar_stock_df.applymap(lambda s: s.lower() if type(s) == str else s)
-
-# set global df column names
-col_names = ['dateOfSale', 'drink', 'price']
-
-# import budapest data
-budapest_df = pd.read_csv('data/budapest.csv.gz'
-                          , compression='gzip'
-                          , header=0
-                          , sep=','
-                          , names=col_names
-                          , parse_dates=['dateOfSale'])
-budapest_df['bar'] = 'budapest'
-budapest_df = budapest_df.loc[budapest_df["dateOfSale"] > date_dict['BUDA_date_max']]
-budapest_max_date = str(budapest_df.dateOfSale.max())
-
-# import london data
-london_df = pd.read_csv('data/london_transactions.csv.gz'
-                        , compression='gzip'
-                        , header=None
-                        , sep='\t'
-                        , names=col_names
-                        , parse_dates=['dateOfSale'])
-london_df['bar'] = 'london'
-london_df = london_df.loc[london_df["dateOfSale"] > date_dict['LON_date_max']]
-london_max_date = str(london_df.dateOfSale.max())
-
-# import new york data
-new_york_df = pd.read_csv('data/ny.csv.gz'
-                          , compression='gzip'
-                          , header=0
-                          , sep=','
-                          , names=col_names
-                          , parse_dates=['dateOfSale'])
-new_york_df['bar'] = 'new york'
-new_york_df = new_york_df.loc[new_york_df["dateOfSale"] > date_dict['NYC_date_max']]
-ny_max_date = str(new_york_df.dateOfSale.max())
-
-# set new dates to limit size of future uploads
-f = open('last_update.txt', 'w')
-f.write(f"NYC_date_max {ny_max_date}\nLON_date_max {london_max_date}\nBUDA_date_max {budapest_max_date}\n")
-date_dict = {}
-f = open("last_update.txt")
-for line in f:
-    k, v = line.split(' ', 1)
-    v = v[:-1]
-    date_dict[k] = v
-f.close
-
-# concat dataframes
-global_df = pd.concat([budapest_df, london_df, new_york_df], ignore_index=True)
-# minor cleaning
-global_df = global_df.reset_index().rename(columns={'index': 'saleID'}).set_index('saleID')
-global_df['price'] = global_df['price'].astype(float)
-global_df = global_df.applymap(lambda s: s.lower() if type(s) == str else s)
-
-# get drinks to query API
-master_drinks = list(set((london_df.drink.unique().tolist())
-                         + (new_york_df.drink.unique().tolist())
-                         + (budapest_df.drink.unique().tolist())))
-
-# apply cocktail_extract function
-counter = 1
-dfs = []
-for drink in master_drinks:
-    # extract data from the offset value
-    df = cocktail_extract(drink)
-    df = df[['idDrink', 'strDrink', 'strCategory', 'strIBA', 'strAlcoholic', 'strGlass', 'dateModified']]
-    # append data
-    dfs.append(df)
-    # print output to see status
-    print(f'Current loop number: {counter} out of {len(master_drinks)}')
-    counter += 1
-
-# combine dfs
-cocktails_df = pd.concat(dfs, ignore_index=True)
-# minor cleaning
-cocktails_df = cocktails_df.sort_values(by='dateModified',ascending=False)
-cocktails_df['dateModified'] = pd.to_datetime(cocktails_df['dateModified'], infer_datetime_format=True)
-cocktails_df = cocktails_df.drop_duplicates(subset=['idDrink', 'strDrink', 'strCategory',
-                                                    'strIBA', 'strAlcoholic', 'strGlass'], keep='first')
-cocktails_df = cocktails_df.applymap(lambda s: s.lower() if type(s) == str else s)
-
-# execute sql script to create tables
-execute_external_sql_script_file('data_tables.sql', 'bar_db')
-
-# populate sql tables
-
-# Connect to sqlite3 database.
-conn = sqlite3.connect('bar_db')
-
-# send each dataframe to sql
-global_df.to_sql('global_sales', conn, if_exists='append', index=True, index_label='saleID')
-bar_stock_df.to_sql('bar_stock', conn, if_exists='append', index=True, index_label='stockID')
-cocktails_df.to_sql('cocktails', conn, if_exists='append', index=False)
-
-# Close the connection object.
-conn.close()
-
-# Create POC Table for analysis
-execute_external_sql_script_file('poc_tables.sql', 'bar_db')
+    f.close
+    
+    # concat dataframes
+    global_df = pd.concat([budapest_df, london_df, new_york_df], ignore_index=True)
+    # minor cleaning
+    global_df = global_df.reset_index().rename(columns={'index': 'saleID'}).set_index('saleID')
+    global_df['price'] = global_df['price'].astype(float)
+    global_df = global_df.applymap(lambda s: s.lower() if type(s) == str else s)
+    
+    # get drinks to query API
+    master_drinks = list(set((london_df.drink.unique().tolist())
+                             + (new_york_df.drink.unique().tolist())
+                             + (budapest_df.drink.unique().tolist())))
+    
+    # apply cocktail_extract function
+    counter = 1
+    dfs = []
+    for drink in master_drinks:
+        # extract data from the offset value
+        df = cocktail_extract(drink)
+        df = df[['idDrink', 'strDrink', 'strCategory', 'strIBA', 'strAlcoholic', 'strGlass', 'dateModified']]
+        # append data
+        dfs.append(df)
+        # print output to see status
+        print(f'Current loop number: {counter} out of {len(master_drinks)}')
+        counter += 1
+    
+    # combine dfs
+    cocktails_df = pd.concat(dfs, ignore_index=True)
+    # minor cleaning
+    cocktails_df = cocktails_df.sort_values(by='dateModified',ascending=False)
+    cocktails_df['dateModified'] = pd.to_datetime(cocktails_df['dateModified'], infer_datetime_format=True)
+    cocktails_df = cocktails_df.drop_duplicates(subset=['idDrink', 'strDrink', 'strCategory',
+                                                        'strIBA', 'strAlcoholic', 'strGlass'], keep='first')
+    cocktails_df = cocktails_df.applymap(lambda s: s.lower() if type(s) == str else s)
+    
+    # execute sql script to create tables
+    execute_external_sql_script_file('data_tables.sql', 'bar_db')
+    
+    # populate sql tables
+    
+    # Connect to sqlite3 database.
+    conn = sqlite3.connect('bar_db')
+    
+    # send each dataframe to sql
+    global_df.to_sql('global_sales', conn, if_exists='append', index=True, index_label='saleID')
+    bar_stock_df.to_sql('bar_stock', conn, if_exists='append', index=True, index_label='stockID')
+    cocktails_df.to_sql('cocktails', conn, if_exists='append', index=False)
+    
+    # Close the connection object.
+    conn.close()
+    
+    # Create POC Table for analysis
+    execute_external_sql_script_file('poc_tables.sql', 'bar_db')
